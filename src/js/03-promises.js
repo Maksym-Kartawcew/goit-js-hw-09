a53e6f8584047bb80679c4ef1aa6a1a9894c91ab
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-05-14 20:39:46 +0200

    Homework9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const timeoutId = setTimeout(() => {
+      clearTimeout(timeoutId);
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
+
+  if ( amount < 0 || delay < 0 || step < 0) {
+    Notify.failure (`Enter positive values`);
+    return
+  }
+  else {
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+});
+
+
+
+
+
+
+
+
+
